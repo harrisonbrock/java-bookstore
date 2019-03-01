4ca6ddae26bb40e9ad5763e66c69ac235a761758
Harrison Brock <harrison@harrisonbrock.com>
2019-03-01 13:02:25 -0500

    added get authors and sections

--- src/main/java/com/harrisonbrock/bookstore/controllers/HomeController.java
+++ src/main/java/com/harrisonbrock/bookstore/controllers/HomeController.java
@@ -2,7 +2,7 @@ package com.harrisonbrock.bookstore.controllers;
 
 import com.harrisonbrock.bookstore.repositories.AuthorRepository;
 import com.harrisonbrock.bookstore.repositories.BookRepository;
-import com.harrisonbrock.bookstore.repositories.SelectionRepository;
+import com.harrisonbrock.bookstore.repositories.SectionRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +13,26 @@ public class HomeController {
 
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
-    private final SelectionRepository selectionRepository;
+    private final SectionRepository sectionRepository;
 
-    public HomeController(BookRepository bookRepository, AuthorRepository authorRepository, SelectionRepository selectionRepository) {
+    public HomeController(BookRepository bookRepository, AuthorRepository authorRepository, SectionRepository sectionRepository) {
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
-        this.selectionRepository = selectionRepository;
+        this.sectionRepository = sectionRepository;
     }
 
     @GetMapping("/books")
     public ResponseEntity<?> getAllBooks() {
         return new ResponseEntity<>(bookRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/authors")
+    public ResponseEntity<?> getAllAuthors() {
+        return new ResponseEntity<>(authorRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/sections")
+    public ResponseEntity<?> getAllSection() {
+        return new ResponseEntity<>(sectionRepository.findAll(), HttpStatus.OK);
     }
 }
